67b3a9bdbbc6e646c673ea72f1bae9c28dcaf7de
Sidney Laryea <slaryea@ghanaports.gov.gh>
2025-04-30 12:37:27 +0000

    First Commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,40 @@
+
+import { useEffect } from "react";
+
+
+export const Navbar = ({menuOpen, setMenuOpen}) => {
+useEffect(() => {
+    document.body.style.overflow = menuOpen ? "hidden" : "auto";
+}, [menuOpen]);
+
+    return (
+        <nav className="fixed top-0 w-full z-40 bg-[rgba(10,10,10,0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg">
+        <div className="max-w-5xl mx-auto px-4">
+        <div className="flex items-center justify-between h-16">
+        <a href="home" className="flex items-center gap-2  font-mono text-xl font-bold text-white">
+            {""}
+            <img src="src/assets/images/logo_new.png" className="w-6 h-6" ></img>Sid<span className="text-blue-500">Consult</span>{""}
+            </a>
+            <div className="w-7 h-5 relative cursor-pointer z-40 md:hidden" onClick={() => setMenuOpen((prev) => !prev)}>
+                &#9776;
+            </div>
+            <div className="hidden md:flex items-center space-x-8">
+            
+            <a href="#Home" className="text-gray-300 hover:text-white transition-colors"
+            >Home </a>
+            
+            <a href="#About" className="text-gray-300 hover:text-white transition-colors"
+            >{""}About {""}</a>
+
+            <a href="#Projects" className="text-gray-300 hover:text-white transition-colors"
+            >Projects</a>
+            
+            <a href="#Contact" className="text-gray-300 hover:text-white transition-colors"
+            >{""}Contact {""}</a>
+            </div>
+
+        </div>
+        </div>
+        </nav>
+    );
+};   
---
+++ src/components/sections/Projects.jsx
@@ -0,0 +1,103 @@
+export const Projects = () => {
+
+return (
+<section id="Projects" className="min-h-screen flex items-center justify-center py-20">
+<div className="max-w-5xl mx-auto px-4">
+    <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text
+                text-transparent text-center ">Featured Projects</h2>
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+    <div  classname="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:
+    shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all">
+    <h3 className="text-xl font-bold mb-2">Library Management System</h3>
+    <p className="text-gray-400 mb-4">
+        A web application that allows users to manage library operations, including book borrowing, 
+        returning, and inventory management. Built with .NET web API,React, Node.js, and Sql.
+    </p>
+        <div>
+            {["React, Node.js", "SQL", ".NET"].map((tech, key) => (
+                <span key={key} className="bg-blue-500 text-white py-1 px-3 rounded-full text-sm font-medium mr-2 mb-2">
+                    {tech}
+                </span>
+            ))}
+        </div>
+        
+            <div className="flex justify-between items-center">    
+                <a href="#" className="text-blue-400 hover:text-blue-300 transition-colors my-4">View Project <span className="ml-1">&rarr;</span> </a>
+            </div>
+    </div>
+
+
+    <div  classname="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:
+    shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all">
+    <h3 className="text-xl font-bold mb-2">Invoice API</h3>
+    <p className="text-gray-400 mb-4">
+        A web application that allows users to manage library operations, including book borrowing, 
+        returning, and inventory management. Built with .NET web API,React, Node.js, and Sql.
+    </p>
+        <div>
+            {["React, Node.js", "SQL", ".NET"].map((tech, key) => (
+                <span key={key} className="bg-blue-500 text-white py-1 px-3 rounded-full text-sm font-medium mr-2 mb-2">
+                    {tech}
+                </span>
+            ))}
+        </div>
+        
+            <div className="flex justify-between items-center">    
+                <a href="#" className="text-blue-400 hover:text-blue-300 transition-colors my-4">View Project <span className="ml-1">&rarr;</span> </a>
+            </div>
+    </div>
+
+    <div  classname="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:
+    shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all">
+    <h3 className="text-xl font-bold mb-2">Inventory Management API</h3>
+    <p className="text-gray-400 mb-4">
+        A web application that allows users to manage library operations, including book borrowing, 
+        returning, and inventory management. Built with .NET web API,React, Node.js, and Sql.
+    </p>
+        <div>
+            {["React, Node.js", "SQL", ".NET"].map((tech, key) => (
+                <span key={key} className="bg-blue-500 text-white py-1 px-3 rounded-full text-sm font-medium mr-2 mb-2">
+                    {tech}
+                </span>
+            ))}
+        </div>
+        
+            <div className="flex justify-between items-center">    
+                <a href="#" className="text-blue-400 hover:text-blue-300 transition-colors my-4">View Project <span className="ml-1">&rarr;</span></a>
+            </div>
+    </div>
+
+
+
+
+    <div  classname="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:
+    shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all">
+    <h3 className="text-xl font-bold mb-2">Time And Attendance Dashboard</h3>
+    <p className="text-gray-400 mb-4">
+        A web application that allows users to manage library operations, including book borrowing, 
+        returning, and inventory management. Built with .NET web API,React, Node.js, and Sql.
+    </p>
+        <div>
+            {["React, Node.js", "SQL", ".NET"].map((tech, key) => (
+                <span key={key} className="bg-blue-500 text-white py-1 px-3 rounded-full text-sm font-medium mr-2 mb-2">
+                    {tech}
+                </span>
+            ))}
+        </div>
+        
+            <div className="flex justify-between items-center">    
+                <a href="#" className="text-blue-400 hover:text-blue-300 transition-colors my-4">View Project <span className="ml-1">&rarr;</span> </a>
+            </div>
+    </div>
+
+
+
+    </div>
+    </div>    
+
+</section>
+);
+
+
+
+};
