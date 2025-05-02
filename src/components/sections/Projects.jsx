26217e4ede05fc6491b0dd2bb851721c02dfa02a
Sidney Laryea <slaryea@ghanaports.gov.gh>
2025-05-02 08:39:39 +0000

    Update On Project Links

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -22,8 +22,9 @@ return (
         </div>
         
             <div className="flex justify-between items-center">    
-                <a href="#" className="text-blue-400 hover:text-blue-300 transition-colors my-4">View Project <span className="ml-1">&rarr;</span> </a>
+                <a href="https://github.com/sidlaryea" className="text-blue-400 hover:text-blue-300 transition-colors my-4">View Project <span className="ml-1">&rarr;</span> </a>
             </div>
+           
     </div>
 
 
@@ -31,8 +32,9 @@ return (
     shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all">
     <h3 className="text-xl font-bold mb-2">Invoice API</h3>
     <p className="text-gray-400 mb-4">
-        A web application that allows users to manage library operations, including book borrowing, 
-        returning, and inventory management. Built with .NET web API,React, Node.js, and Sql.
+    A RESTful API built with ASP.NET Web API that allows businesses to manage invoicing operations efficiently. 
+    Key features include creating, retrieving, updating, and deleting invoices; managing clients; calculating totals with tax and discounts; and tracking payment status. 
+    Designed for integration with accounting systems or business apps, this API ensures secure, scalable, and streamlined invoice processing.
     </p>
         <div>
             {["React, Node.js", "SQL", ".NET"].map((tech, key) => (
@@ -43,7 +45,7 @@ return (
         </div>
         
             <div className="flex justify-between items-center">    
-                <a href="#" className="text-blue-400 hover:text-blue-300 transition-colors my-4">View Project <span className="ml-1">&rarr;</span> </a>
+                <a href="https://github.com/sidlaryea" className="text-blue-400 hover:text-blue-300 transition-colors my-4">View Project <span className="ml-1">&rarr;</span> </a>
             </div>
     </div>
 
@@ -63,7 +65,7 @@ return (
         </div>
         
             <div className="flex justify-between items-center">    
-                <a href="#" className="text-blue-400 hover:text-blue-300 transition-colors my-4">View Project <span className="ml-1">&rarr;</span></a>
+                <a href="https://github.com/sidlaryea" className="text-blue-400 hover:text-blue-300 transition-colors my-4">View Project <span className="ml-1">&rarr;</span></a>
             </div>
     </div>
 
@@ -86,7 +88,7 @@ return (
         </div>
         
             <div className="flex justify-between items-center">    
-                <a href="#" className="text-blue-400 hover:text-blue-300 transition-colors my-4">View Project <span className="ml-1">&rarr;</span> </a>
+                <a href="https://github.com/sidlaryea" className="text-blue-400 hover:text-blue-300 transition-colors my-4">View Project <span className="ml-1">&rarr;</span> </a>
             </div>
     </div>
 
